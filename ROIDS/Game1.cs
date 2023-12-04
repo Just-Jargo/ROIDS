21aaff2532d326485729fe38221cd3b37a320bea
Richard Mwanza <ec1865584@edinburghcollege.ac.uk>
2023-12-04 15:57:07 +0000

    Add project files.

---
+++ ROIDS/Game1.cs
@@ -0,0 +1,139 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+using System;
+using System.Collections.Generic;
+using System.IO;
+
+
+//CTRL + Double R 
+namespace ROIDS
+{
+    public class Game1 : Game
+    {
+        private GraphicsDeviceManager _graphics;
+        private SpriteBatch _spriteBatch;
+
+
+
+        Texture2D txrShip, txrAsteroids, txrParticle;
+
+        string highscore, highScoreName, highscoreFile;
+        int highScoreCount, currentScore;
+
+        List<Sprite> spriteList = new List<Sprite>();
+
+        public Game1()
+        {
+            _graphics = new GraphicsDeviceManager(this);
+            Content.RootDirectory = "Content";
+            IsMouseVisible = true;
+        }
+
+        protected override void Initialize()
+        {
+            highscore = Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData) + "\\ROIDS";
+            highscoreFile = highscore + "\\highscore.sav";
+
+
+            
+            base.Initialize();
+
+            LoadhighScore();
+
+            highScoreCount = 99;
+            highScoreName = "Dave";
+            saveHighscore();
+
+        }
+
+        protected override void LoadContent()
+        {
+            _spriteBatch = new SpriteBatch(GraphicsDevice);
+
+            txrShip = Content.Load<Texture2D>("SpriteShip");
+            txrParticle = Content.Load<Texture2D>("SpriteParticle");
+            txrAsteroids = Content.Load<Texture2D>("SpriteRocks");
+
+            LoadhighScore();
+
+
+        }
+
+        protected override void Update(GameTime gameTime)
+        {
+            KeyboardState currentKeys = Keyboard.GetState();
+
+            if (currentKeys.IsKeyDown(Keys.Space)) Exit();
+
+            foreach(Sprite everySprite in spriteList) everySprite.Update(gameTime);
+         
+
+            base.Update(gameTime);
+        }
+
+        protected override void Draw(GameTime gameTime)
+        {
+            GraphicsDevice.Clear(Color.DarkSeaGreen);
+
+            _spriteBatch.Begin();
+
+            foreach (Sprite everySprite in spriteList) everySprite.Draw(gameTime, _spriteBatch);
+
+            _spriteBatch.End();
+
+            base.Draw(gameTime);
+        }
+
+        //Void is the same as local in C#
+        void saveHighscore()
+        {
+            //if the directory doesn't exist, then make it.
+            if (!Directory.Exists(highscore)) Directory.CreateDirectory(highscore);
+            File.WriteAllText(highscoreFile, highScoreName);
+            File.AppendAllText(highscoreFile, "\n" + highScoreCount);
+        }
+
+        void LoadhighScore()
+        {
+            string[] fileStrings;
+
+            if (File.Exists(highscoreFile))
+            {
+                fileStrings = File.ReadAllLines(highscoreFile);
+
+                if (fileStrings.Length != 2)
+                {
+                    makeDefaultFile();
+                }
+                else
+                {
+                    if (fileStrings[0].Length > 3) highScoreName = fileStrings[0].Substring(0, 3);
+                    else highScoreName = fileStrings[0];
+
+                    if(highScoreName == "") highScoreName = "---"; 
+
+                    if(int.TryParse(fileStrings[1], out highScoreCount)) highScoreCount = 0;
+                }
+            }
+            else
+            {
+                makeDefaultFile();
+            }
+
+           
+        }
+
+        void makeDefaultFile()
+        {
+            highScoreName = "---";
+            highScoreCount = 0;
+            saveHighscore();
+
+        }
+
+
+
+    }
+
+}
---
+++ ROIDS/Sprite.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using Microsoft.Xna.Framework.Input;
+
+
+namespace ROIDS
+{
+    //Allows for other classes to use.
+    public class Sprite
+    {
+        public Texture2D spriteTxr;
+        public Vector2 spritePos, spriteVelocity, spriteOrigin;
+        public float spriteRadius, spriteRotation;
+        public bool canCollide;
+        public Rectangle spriteDrawRect, spriteSourceRect;
+
+        //Draws in sprites every frame currently
+        //Also can be used to set up a spritesheet
+
+
+        public Sprite(Texture2D newSpriteTxr, Vector2 startPos)
+        { 
+        
+            spriteTxr = newSpriteTxr;
+            spritePos = startPos;
+
+            //Auto sets the speed of stuff to zero
+            spriteVelocity = Vector2.Zero;
+            spriteRadius =  spriteTxr.Width / 4.0f;
+            spriteRotation = 0.0f;
+
+            canCollide = true;
+            spriteSourceRect = spriteTxr.Bounds;
+            spriteOrigin = new Vector2(spriteTxr.Width  /2.0f, spriteTxr.Height / 2.0f);
+
+
+        }
+
+        public void Update(GameTime gameTime)
+        {
+            spritePos += spriteVelocity * (float)gameTime.ElapsedGameTime.TotalSeconds;
+            spriteDrawRect = new Rectangle((int)spritePos.X, (int)spritePos.Y, spriteTxr.Width, spriteTxr.Height);
+
+        }
+
+        public void Draw(GameTime gameTime, SpriteBatch spriteBatch)
+        {
+            spriteBatch.Draw(spriteTxr, spriteDrawRect, spriteSourceRect, Color.White, spriteRotation, 
+                spriteOrigin, SpriteEffects.None, 0.0f);
+        }
+
+    }
+}
